fddd8edb7ac503df7966c8c737200227779fa81b
JrgenGNielsen <jgn@dbc.dk>
2014-01-13 15:51:47 +0100

    first commit

---
+++ js/bibdk_about_author.js
@@ -0,0 +1,41 @@
+(function ($) {
+
+    /** Insert about_author results */
+    Drupal.addAboutAuthor = function (about_author) {
+        if ( about_author.error ) {
+          $('.about-author-load[data-query=' + about_author.query + ']').replaceWith(about_author.error);
+        }
+        if ( about_author.list ) {
+          $('.about-author-load[data-query=' + about_author.query + ']').replaceWith(about_author.list);
+          $('.about-author-more[data-query=' + about_author.query + ']').removeClass('visuallyhidden');
+        }
+    },
+    Drupal.loadAboutAuthor = function (element) {
+        var query = $(element).attr('data-about-author-query');
+        /* Add throbber*/
+        $(element).addClass('ajax-progress');
+        $(element).html('<span class="throbber">&nbsp;</span>');
+
+        /* Call ajax */
+        var request = $.ajax({
+            url:Drupal.settings.basePath + 'about_author/ajax',
+            type:'POST',
+            data:{
+                query:query
+            },
+            dataType:'json',
+            success:Drupal.addAboutAuthor
+        });
+    };
+
+    /** Get holdingstatus via ajax */
+    Drupal.behaviors.aboutAuthor = {
+        attach:function (context) {
+            $('.about-author-load', context).each(function (i, element) {
+                Drupal.loadAboutAuthor(element);
+            });
+        },
+        detach:function (context) {
+        }
+    };
+}(jQuery));
