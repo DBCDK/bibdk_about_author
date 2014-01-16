9521ea1ba3ddece74e031df1bf222d84e5aff7f3
JrgenGNielsen <jgn@dbc.dk>
2014-01-16 14:56:51 +0100

    finish tests, and polishing

--- theme/bibdk-about-author-links.tpl.php
+++ theme/bibdk-about-author-links.tpl.php
@@ -2,6 +2,13 @@
 /*
  * Template for bibdk "about author" links
  */
+
+foreach ( $about_author_links as $id => $link ) {
+  $title = t("bibdk_about_author_read @display", array('@display' => $link['#title']), array('context' => 'bibdk_about_author'));
+  $about_author_links[$id]['#title'] = $title;
+  $about_author_links[$id]['#options']['title'] = $title;
+}
+
 ?>
 
 <div class="about-author-links">
